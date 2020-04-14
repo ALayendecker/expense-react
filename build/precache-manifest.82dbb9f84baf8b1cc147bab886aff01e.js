5cadc6e474975fd74a7205e08aa355a98d2831b6
Andrew Layendecker <andrewlayendecker@gmail.com>
2020-04-14 13:53:30 -0500

    testing something/build

---
+++ build/precache-manifest.82dbb9f84baf8b1cc147bab886aff01e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8508d558ead1f75ab12ec1563ff4ecd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d05b7d990051429fbd0",
+    "url": "/static/css/main.caf1db2f.chunk.css"
+  },
+  {
+    "revision": "264b455111028a3d4eab",
+    "url": "/static/js/2.6ab39766.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6ab39766.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d05b7d990051429fbd0",
+    "url": "/static/js/main.41317a6e.chunk.js"
+  },
+  {
+    "revision": "98894c03815665b4dc90",
+    "url": "/static/js/runtime-main.53a2868f.js"
+  }
+]);
